f0dc1f1b8acd41290c3cd0962d5bbdd24f595208
irshad0505 <irshadibz786@gmail.com>
2022-03-04 18:19:21 +0530

    some changes

--- general/models.py
+++ general/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class UserModel(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    photos = models.ImageField(upload_to='images')
     email = models.EmailField(max_length=100)
     status = models.BooleanField(default=True)
     created_on = models.DateTimeField(auto_now_add=True)
